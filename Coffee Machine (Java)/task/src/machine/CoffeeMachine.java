d81cdc2122bb4c122a555022cb3a364f7d505b62
Waiman Yau <wyau121@gmail.com>
2023-11-08 17:22:28 +0000

    Asking for input and calculating quantities of water, milk, coffee beans

--- Coffee Machine (Java)/task/src/machine/CoffeeMachine.java
+++ Coffee Machine (Java)/task/src/machine/CoffeeMachine.java
@@ -1,7 +1,14 @@
 package machine;
 
+import java.util.Scanner;
+
 public class CoffeeMachine {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int water = 200;
+        int milk = 50;
+        int coffeeBeans = 15;
 
         System.out.println("Starting to make a coffee");
         System.out.println("Grinding coffee beans");
@@ -10,5 +17,13 @@ public class CoffeeMachine {
         System.out.println("Pouring coffee into the cup");
         System.out.println("Pouring some milk into the cup");
         System.out.println("Coffee is ready!");
+
+        System.out.println("How many cups of coffee will you need:");
+        System.out.print("> ");
+        int cupsOfCoffeeNeeded = scanner.nextInt();
+        System.out.println("For " + cupsOfCoffeeNeeded + " cups of coffee you will need:");
+        System.out.println(cupsOfCoffeeNeeded * water + " ml of water");
+        System.out.println(cupsOfCoffeeNeeded * milk + " ml of milk");
+        System.out.println(cupsOfCoffeeNeeded * coffeeBeans + " g of coffee beans");
     }
 }
